de8de4e31767b8511184bc527d26a8702d0a782d
Awais Tahir <theowais456@gmail.com>
2025-03-26 00:51:02 +0500

    removed sidebar

--- HospitalManagementSystem/obj/Debug/net8.0/HospitalManagementSystem.AssemblyInfo.cs
+++ HospitalManagementSystem/obj/Debug/net8.0/HospitalManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HospitalManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b81499a811e020cbe29f3655ee8aae9af4f1109")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79eb075b5dbd6462953b2dc0c4b489e8b1bc8845")]
 [assembly: System.Reflection.AssemblyProductAttribute("HospitalManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HospitalManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
