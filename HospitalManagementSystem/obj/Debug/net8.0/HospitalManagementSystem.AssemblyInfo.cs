359ad6ea3c28e87340a152b326bf82f77aa44362
WaqasSiddique02 <siddique02mwaqas@gmail.com>
2025-03-27 22:35:09 +0500

    connected user with backend

--- HospitalManagementSystem/obj/Debug/net8.0/HospitalManagementSystem.AssemblyInfo.cs
+++ HospitalManagementSystem/obj/Debug/net8.0/HospitalManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HospitalManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+452e6799b7fe1d571948953f3a9127d6181c073f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49e6c02d5055bc8c5e0478a1ea9bb5fb0d575bc7")]
 [assembly: System.Reflection.AssemblyProductAttribute("HospitalManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HospitalManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
